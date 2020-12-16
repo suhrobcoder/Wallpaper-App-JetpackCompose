bfdd6514109ed6a70bde2fcafcf0ecc2cbf1ac77
suhrobcoder <suhrobcoder@gmail.com>
2020-12-17 04:57:48 +0500

    setup-navigation

--- app/src/main/java/uz/suhrob/wallpaperapp/presentation/MainActivity.kt
+++ app/src/main/java/uz/suhrob/wallpaperapp/presentation/MainActivity.kt
@@ -2,39 +2,13 @@ package uz.suhrob.wallpaperapp.presentation
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.platform.setContent
-import androidx.compose.ui.tooling.preview.Preview
 import dagger.hilt.android.AndroidEntryPoint
-import uz.suhrob.wallpaperapp.presentation.theme.WallpaperAppTheme
+import uz.suhrob.wallpaperapp.R
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContent {
-            WallpaperAppTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(color = MaterialTheme.colors.background) {
-                    Greeting("Android")
-                }
-            }
-        }
-    }
-}
-
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    WallpaperAppTheme {
-        Greeting("Android")
+        setContentView(R.layout.activity_main)
     }
 }
