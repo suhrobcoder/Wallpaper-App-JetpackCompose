70e887974088da4900719fac7c09fbd59ed17d69
suhrobcoder <suhrobcoder@gmail.com>
2020-12-25 13:51:38 +0500

    removed database, because i cannot add paging with database

--- app/src/main/java/uz/suhrob/wallpaperapp/other/Constants.kt
+++ app/src/main/java/uz/suhrob/wallpaperapp/other/Constants.kt
@@ -2,8 +2,6 @@ package uz.suhrob.wallpaperapp.other
 
 const val BASE_URL = "https://api.pexels.com/v1/"
 
-const val DATABASE_NAME = "photos.db"
-
 const val PHOTO_PER_PAGE = 20
 const val STARTING_PAGE = 1
 
